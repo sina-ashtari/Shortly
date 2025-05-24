e16400fb87c72868d4dc843fa038d077b86efca8
sina-ashtari <sinaa1383@gmail.com>
2025-05-24 21:42:43 +0330

    Feat : Add Base62 encoding extension class

---
+++ Shortly.Core/EncodeToBase62Extension.cs
@@ -0,0 +1,18 @@
+﻿namespace Shortly.Core;
+
+public static class EncodeToBase62Extension
+{
+    private const string AlphaNum = "0123456789" + "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "abcdefghijklmnopqrstuvwxyz";
+
+    public static string EncodeToBase62(this long number)
+    {
+        if (number == 0) return AlphaNum[0].ToString();
+        var result = new Stack<char>();
+        while (number > 0)
+        {
+            result.Push(AlphaNum[(int)(number % 62)]);
+            number /= 62;
+        }
+        return new string(result.ToArray());
+    }
+}
