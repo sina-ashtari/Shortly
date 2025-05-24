58f613f798e39c50df1303600c462bd426aa1da0
sina-ashtari <sinaa1383@gmail.com>
2025-05-24 21:43:18 +0330

    Feat : Add Shortened url generator extension

---
+++ Shortly.Core/ShortUrlGenerator.cs
@@ -0,0 +1,9 @@
+﻿namespace Shortly.Core;
+
+public class ShortUrlGenerator(TokenProvider tokenProvider)
+{
+    public string GenerateUniqueUrl()
+    {
+        return tokenProvider.GetToken().EncodeToBase62();
+    }
+}
