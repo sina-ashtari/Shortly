572dd9eb30d51dc8977bd5f4ee00390183d3b1d9
sina-ashtari <sinaa1383@gmail.com>
2025-05-24 21:54:40 +0330

    Feat : Add Token Range record and its provider to assigning start and end token range

---
+++ Shortly.Core/TokenRange.cs
@@ -0,0 +1,14 @@
+﻿namespace Shortly.Core;
+
+public record TokenRange
+{
+    public long Start { get; }
+    public long End { get; }
+    
+    public TokenRange(long start, long end)
+    {
+        Start = start;
+        End = end;
+    }
+    
+}
