cefb4b40fb1d4d8384067eb954c08c30b9ac7c31
sina-ashtari <sinaa1383@gmail.com>
2025-05-26 14:08:57 +0330

    Feat: Add shortened url data class

---
+++ Shortly.Core/Url/ShortenedUrl.cs
@@ -0,0 +1,17 @@
+﻿namespace Shortly.Core.Url;
+
+public class ShortenedUrl
+{
+    public ShortenedUrl(Uri longUrl, string shortUrl, string CreatedBy, DateTimeOffset CreatedOn)
+    {
+        LongUrl = longUrl;
+        ShortUrl = shortUrl;
+        this.CreatedOn = CreatedOn;
+        this.CreatedBy = CreatedBy;
+    }
+
+    public Uri LongUrl { get; set; }
+    public string ShortUrl { get; set; }
+    public string CreatedBy { get;  }
+    public DateTimeOffset CreatedOn{ get;  }
+}
