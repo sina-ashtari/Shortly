e3d0503ab20b7f2ed27ddabcf87bfdf2903ae4ca
sina-ashtari <sinaa1383@gmail.com>
2025-05-26 13:56:35 +0330

    Feat : Add Url Handler functionality based on in-memory data

---
+++ Shortly.Core/Url/Add/AddUrlHandler.cs
@@ -0,0 +1,22 @@
+﻿namespace Shortly.Core.Url.Add;
+
+public class AddUrlHandler
+{
+    private readonly ShortUrlGenerator _shortUrlGenerator;
+    private readonly IUrlDataStore _urlDataStore;
+    private readonly TimeProvider _timeProvider;
+
+    public AddUrlHandler(ShortUrlGenerator shortUrlGenerator, IUrlDataStore urlDataStore, TimeProvider timeProvider)
+    {
+        _shortUrlGenerator = shortUrlGenerator;
+        _urlDataStore = urlDataStore;
+        _timeProvider = timeProvider;
+    }
+
+    public async Task<AddUrlResponse> HandleAsync(AddUrlRequest request, CancellationToken token)
+    {
+        var shortened = new ShortenedUrl(request.LongUrl, _shortUrlGenerator.GenerateUniqueUrl(), request.CreatedBy, _timeProvider.GetUtcNow());
+        await _urlDataStore.AddAsync(shortened, token);
+        return new AddUrlResponse(request.LongUrl, shortened.ShortUrl);
+    }
+}
