a6b667ff2fc63ea4416e23f3e703a7e2b2aebf57
sina-ashtari <sinaa1383@gmail.com>
2025-05-26 18:01:38 +0330

    Feat : Add empty check for user

--- Shortly.Core/Url/Add/AddUrlHandler.cs
+++ Shortly.Core/Url/Add/AddUrlHandler.cs
@@ -13,8 +13,10 @@ public class AddUrlHandler
         _timeProvider = timeProvider;
     }
 
-    public async Task<AddUrlResponse> HandleAsync(AddUrlRequest request, CancellationToken token)
+    public async Task<Result<AddUrlResponse>> HandleAsync(AddUrlRequest request, CancellationToken token)
     {
+        if (string.IsNullOrEmpty(request.CreatedBy)) return Errors.MissingCreatedBy;
+        
         var shortened = new ShortenedUrl(request.LongUrl, _shortUrlGenerator.GenerateUniqueUrl(), request.CreatedBy, _timeProvider.GetUtcNow());
         await _urlDataStore.AddAsync(shortened, token);
         return new AddUrlResponse(request.LongUrl, shortened.ShortUrl);
