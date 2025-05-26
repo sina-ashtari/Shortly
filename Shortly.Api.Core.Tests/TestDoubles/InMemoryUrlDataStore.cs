e0c5e8a73d8cc4ff2f442887f3f026e6ca588395
sina-ashtari <sinaa1383@gmail.com>
2025-05-26 14:09:45 +0330

    Feat : Add in memory data store implementation

---
+++ Shortly.Api.Core.Tests/TestDoubles/InMemoryUrlDataStore.cs
@@ -0,0 +1,13 @@
+﻿using Shortly.Core.Url;
+using Shortly.Core.Url.Add;
+
+namespace Shortly.Api.Core.Tests.TestDoubles;
+
+public class InMemoryUrlDataStore : Dictionary<string, ShortenedUrl>, IUrlDataStore
+{
+    public Task AddAsync(ShortenedUrl shortened, CancellationToken token)
+    {
+        Add(shortened.ShortUrl, shortened);
+        return Task.CompletedTask;
+    }
+}
