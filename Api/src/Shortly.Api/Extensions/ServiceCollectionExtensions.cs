881dc02ac2f68c4286180b943e4194a7d4edbffc
sina-ashtari <sinaa1383@gmail.com>
2025-05-27 13:07:59 +0330

    Refactor : Move related tests projects under tests folder

---
+++ Api/src/Shortly.Api/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Shortly.Core;
+using Shortly.Core.Url;
+using Shortly.Core.Url.Add;
+
+namespace Shortly.Api.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddUrlFeature(this IServiceCollection services)
+    {
+        services.AddScoped<AddUrlHandler>();
+        services.AddSingleton<TokenProvider>(_ =>
+        {
+            var tokenProvider = new TokenProvider();
+            tokenProvider.AssignRange(1, 1000);
+            return tokenProvider;
+        });
+        services.AddScoped<ShortUrlGenerator>();
+        services.AddScoped<IUrlDataStore, InMemoryUrlDataStore>();
+        return services;
+    }
+    
+}
+public class InMemoryUrlDataStore: Dictionary<string, ShortenedUrl>, IUrlDataStore
+{
+    public Task AddAsync(ShortenedUrl shortened, CancellationToken token)
+    {
+        Add(shortened.ShortUrl, shortened);
+        return Task.CompletedTask;
+    }
+}
